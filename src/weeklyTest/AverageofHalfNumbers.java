51cc015dd5b2b19739754a488e49db6252a1ac39
SaiKarthikeyaRachamalla <rskarthik2610@gmail.com>
2024-10-16 21:42:34 +0530

    verfiying the adding the new file to the old repositroy

---
+++ src/weeklyTest/AverageofHalfNumbers.java
@@ -0,0 +1,24 @@
+package weeklyTest;
+
+import java.util.Scanner;
+
+public class AverageofHalfNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the how number are there");
+		Scanner scanner = new Scanner(System.in);
+		int n=scanner.nextInt();
+		int ar[]=new int[n];
+		System.out.println("Enter the number with space origniated");
+		for(int i=0;i<ar.length;i++) {
+			ar[i]=scanner.nextInt();
+		}
+		int avg=0;
+		for(int i=0;i<ar.length/2;i++) {
+			avg+=ar[i];
+		}
+		System.out.println("The avrage of half of the number is "+avg/(ar.length/2));
+		
+	}
+
+}
